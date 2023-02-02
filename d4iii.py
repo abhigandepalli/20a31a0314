596435d8753e7fb5a14fa28c6d27459979ce7722
Abhishekraj Gandepalli <124232206+abhigandepalli@users.noreply.github.com>
2023-02-02 11:34:26 +0530

    Add files via upload

---
+++ d4iii.py
@@ -0,0 +1,14 @@
+text='india'
+index=0
+for i in text:
+    print("(text[",index,"]=",1)
+    index +=1
+
+text='India is great'
+print(text.title())
+
+text='India is great'
+print(text.swapcase())
+
+text='India,Is,Great'
+print(text.split())
