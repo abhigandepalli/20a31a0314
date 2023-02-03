f54d7c1b59d5f5fd761e966320bc7c97405a12b7
Abhishekraj Gandepalli <124232206+abhigandepalli@users.noreply.github.com>
2023-02-03 10:14:22 +0530

    Add files via upload

---
+++ d5i.py
@@ -0,0 +1,14 @@
+def total(a,b):
+    result1=a+b
+    result2=a-b
+    result3=a*b
+    result4=a^b
+    result5=a/b
+    print("function result is",result1)
+    print("function result is",result2)
+    print("function result is",result3)
+    print("function result is",result4)
+    print("function result is",result5)
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+total(a,b)
